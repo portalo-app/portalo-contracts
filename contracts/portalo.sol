72ec74949d3e03b35f4bb56bc6441968f323fef6
Pablo Corrado <pcorrado@neopower.digital>
2024-08-04 04:25:34 -0300

    refactor: remove open zeppelin import

--- contracts/portalo.sol
+++ contracts/portalo.sol
@@ -2,8 +2,6 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-import "@openzeppelin/contracts/utils/Strings.sol";
-
 interface IVerifier {
     function verify(bytes calldata _proof, bytes32[] calldata _publicInputs) external view returns (bool);
 }
