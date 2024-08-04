962240d1b880484cb2c67948c025f14b8a39417d
juanarancibia <arancibia.juan22@gmail.com>
2024-08-03 21:30:20 -0300

    feat: contract iteration

--- src/Portalo.sol
+++ src/Portalo.sol
@@ -5,13 +5,16 @@ pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IVerifier {
-    function verify(bytes calldata _proof, bytes32[] calldata _publicInputs) external view returns (bool);
+    function verify(
+        bytes calldata _proof,
+        bytes32[] calldata _publicInputs
+    ) external view returns (bool);
 }
 
 contract Portalo {
-
     struct Profile {
-        string profileId;
+        uint8 profileId;
+        uint8 nonce;
         string profileEncryptionKey;
         string encryptedData;
         bool isPublic;
@@ -24,50 +27,38 @@ contract Portalo {
 
     IVerifier verifier;
 
-    mapping(address => string[]) public profileIdsByOwners;
-    mapping(string => Profile) public profileByProfileId;
-    mapping(string => mapping(address => string)) public profileSharedAccounts;
+    mapping(address => uint8[]) public profileIdsByOwners;
+    mapping(uint8 => Profile) public profileByProfileId;
 
     constructor(address _verifierAddress) {
         verifier = IVerifier(_verifierAddress);
     }
 
-    function saveProfile(Profile memory profile, bytes memory _proof, bytes32[] memory publicInput) external payable {
-        // _verifyProof(_proof, publicInput);
+    function saveProfile(
+        Profile memory profile,
+        bytes memory _proof,
+        bytes32[] memory publicInput
+    ) external payable {
+        _verifyProof(_proof, publicInput);
 
         profileIdsByOwners[msg.sender].push(profile.profileId);
         profileByProfileId[profile.profileId] = profile;
     }
 
-    // encryptedPEK -> profileEncryptionKey encrypted for that publicKey
-    function grantAccess(address recipient, string memory profileId, string memory encryptedPEK,bytes memory _proof, bytes32[] memory publicInput) external payable {
-        // _verifyProof(_proof, publicInput);
-
-        profileSharedAccounts[profileId][recipient] = encryptedPEK;
-    }
-
-    function sharePublicProfile(Profile memory profile, bytes memory _proof, bytes32[] memory publicInput) external payable {
-        // _verifyProof(_proof, publicInput);
-
-        profileByProfileId[profile.profileId] = profile;
-        profileByProfileId[profile.profileId].isPublic = true;
-
-    }
-
-    function revokeAccess(address recipient, string memory profileId, bytes memory _proof, bytes32[] memory publicInput) external payable {
-        // _verifyProof(_proof, publicInput);
-
-        delete profileSharedAccounts[profileId][recipient];
-    }
-
-    function resetEncryptionKey(Profile memory profile, bytes memory _proof, bytes32[] memory publicInput) external payable {
-        // _verifyProof(_proof, publicInput);
+    function resetEncryptionKey(
+        Profile memory profile,
+        bytes memory _proof,
+        bytes32[] memory publicInput
+    ) external payable {
+        _verifyProof(_proof, publicInput);
 
         profileByProfileId[profile.profileId] = profile;
     }
 
-  
-    // function _verifyProof(bytes memory proof, bytes32[] memory publicInput) internal view {
-    //     require(verifier.verify(proof, publicInput), "Invalid proof");
-    // }
+    function _verifyProof(
+        bytes memory proof,
+        bytes32[] memory publicInput
+    ) internal view {
+        require(verifier.verify(proof, publicInput), "Invalid proof");
+    }
 }
