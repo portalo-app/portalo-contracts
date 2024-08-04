a922f92a466e6ff6859a9b7b26778e7e822756c4
juanarancibia <arancibia.juan22@gmail.com>
2024-08-04 00:48:51 -0300

    feat: add editing

--- contracts/portalo.sol
+++ contracts/portalo.sol
@@ -5,24 +5,17 @@ pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IVerifier {
-    function verify(
-        bytes calldata _proof,
-        bytes32[] calldata _publicInputs
-    ) external view returns (bool);
+    function verify(bytes calldata _proof, bytes32[] calldata _publicInputs) external view returns (bool);
 }
 
 contract Portalo {
+
     struct Profile {
         uint8 profileId;
         uint8 nonce;
         string profileEncryptionKey;
         string encryptedData;
         bool isPublic;
-    }
-
-    struct SharedProfile {
-        address recipient;
-        string encryptedProfileEncryptionKey;
     }
 
     IVerifier verifier;
@@ -34,31 +27,18 @@ contract Portalo {
         verifier = IVerifier(_verifierAddress);
     }
 
-    function saveProfile(
-        Profile memory profile,
-        bytes memory _proof,
-        bytes32[] memory publicInput
-    ) external payable {
+    function saveProfile(Profile memory profile, bytes memory _proof, bytes32[] memory publicInput, bool isEditing) external payable {
         _verifyProof(_proof, publicInput);
 
-        profileIdsByOwners[msg.sender].push(profile.profileId);
-        profileByProfileId[profile.profileId] = profile;
-    }
-
-    function resetEncryptionKey(
-        Profile memory profile,
-        bytes memory _proof,
-        bytes32[] memory publicInput
-    ) external payable {
-        _verifyProof(_proof, publicInput);
+        if (!isEditing) {
+            // ToDo: Encrypt this array
+            profileIdsByOwners[msg.sender].push(profile.profileId);
+        }
 
         profileByProfileId[profile.profileId] = profile;
     }
-
-    function _verifyProof(
-        bytes memory proof,
-        bytes32[] memory publicInput
-    ) internal view {
+  
+    function _verifyProof(bytes memory proof, bytes32[] memory publicInput) internal view {
         require(verifier.verify(proof, publicInput), "Invalid proof");
     }
 }
